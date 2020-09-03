0ca08a001b385ba190b83f961c05f8983257a2b4
Sciarra.Martin.69650 <sciarra.martin@gmail.com>
2020-09-02 23:13:56 -0300

    proyecto finalizado-- formulario agrega productos y elimina utilizando javascript puro, no posee base de datos.

--- app.js
+++ app.js
@@ -13,10 +13,27 @@ class UI{
   addProduct(product){
     const list = document.getElementById("product-list");
     const elementHTML = document.createElement("div");
+    elementHTML.innerHTML = `
+      <div class="card text-center mb-4">
+        <div class="card-body">
+          <strong>Producto: </strong> ${product.name}
+          <strong>Precio: </strong> ${product.price}
+          <strong>year: </strong> ${product.year}
+          <a href="#" class=" btn btn-danger" name="delete" >Borrar</a>
+        </div>
+      </div>
+    `;
+    list.appendChild(elementHTML);
     
   }
-  deleteProduct(){
+  resetForm(){
+    document.getElementById("product-form").reset();
+  }
 
+  deleteProduct(element){
+    if (element.name === "delete"){
+      element.parentElement.parentElement.parentElement.remove();
+    } 
   }
   showMessage(){
 
@@ -31,5 +48,15 @@ document.getElementById("product-form").addEventListener("submit", function(e){
 
   console.log(new Product(name, price, year));
   const product = new Product(name, price, year);
+
+  const ui = new UI;
+  ui.addProduct(product);
+  ui.resetForm();
   e.preventDefault();
+
+});
+
+document.getElementById("product-list").addEventListener("click", function(e){
+    const ui = new UI;
+    ui.deleteProduct(e.target);
 });
